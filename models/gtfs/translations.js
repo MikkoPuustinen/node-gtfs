f475f500f54045094dbc7c971047cd18fb4fe0b8
MikkoPuustinen <mikkowr@gmail.com>
2021-07-13 12:17:16 +0300

    translation schema

--- models/gtfs/translations.js
+++ models/gtfs/translations.js
@@ -2,22 +2,12 @@ module.exports = {
   filenameBase: 'translations',
   schema: [
     {
-      name: 'id',
-      type: 'integer',
+      name: 'trans_id',
+      type: 'varchar(2047)',
       primary: true
     },
     {
-      name: 'table_name',
-      type: 'varchar(255)',
-      required: true
-    },
-    {
-      name: 'field_name',
-      type: 'varchar(255)',
-      required: true
-    },
-    {
-      name: 'language',
+      name: 'lang',
       type: 'varchar(255)',
       required: true
     },
@@ -25,18 +15,6 @@ module.exports = {
       name: 'translation',
       type: 'varchar(2047)',
       required: true
-    },
-    {
-      name: 'record_id',
-      type: 'varchar(255)'
-    },
-    {
-      name: 'record_sub_id',
-      type: 'varchar(255)'
-    },
-    {
-      name: 'field_value',
-      type: 'varchar(2047)'
     }
   ]
 };
